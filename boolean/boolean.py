1ca1690d793aad0d2707802b881d914ff759cea5
RomanPrograma <126255710+RomanPrograma@users.noreply.github.com>
2023-03-24 23:37:23 +0200

    ex_3 done

--- boolean/boolean.py
+++ boolean/boolean.py
@@ -1,1 +1,10 @@
-
+number_of_meters = int(input("number_of_meters   "))
+value = float(input("1-convert to miles \n2-convert to inches \n3-convert to yards \n"))
+if value ==1:
+    print(number_of_meters*0.000621371)
+elif value ==2:
+    print(number_of_meters*39.370)
+elif value ==3:
+    print(number_of_meters*39.370)
+else:
+    print("err")
