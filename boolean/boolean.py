d15dbec394202a3bd1da47820762bb69ff344372
RomanPrograma <126255710+RomanPrograma@users.noreply.github.com>
2023-03-24 23:25:32 +0200

    EX_2 done

--- boolean/boolean.py
+++ boolean/boolean.py
@@ -1,4 +1,22 @@
 first_number = int(input("suggest a number 1   "))
 second_number = int(input("suggest a number 2   "))
 third_number = int(input("suggest a number 3   "))
-value = input("")
+value = int(input("1-maximum of three \n2-minimum of three \n3-arithmetic mean of three \n"))
+if value ==1:
+    if first_number > second_number > third_number:
+        print(first_number)
+    elif second_number > third_number > first_number:
+        print(second_number)
+    elif third_number > second_number > first_number:
+        print(third_number)
+elif value ==2:
+    if first_number < second_number < third_number:
+        print(first_number)
+    elif second_number < first_number < third_number:
+        print(second_number)
+    elif third_number < first_number < second_number:
+        print(third_number)
+elif value ==3:
+    print((first_number + second_number + third_number) / 3)
+else:
+    print("err")
