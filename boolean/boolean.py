6f5db399cbedc072b17eec14f7e9d24dce47773b
RomanPrograma <126255710+RomanPrograma@users.noreply.github.com>
2023-03-24 20:18:05 +0200

    ex_3 done

--- boolean/boolean.py
+++ boolean/boolean.py
@@ -1,1 +1,8 @@
+number_1 = int(input("suggest a number 1   "))
+number_2 = int(input("suggest a number 2   "))
+if number_1 > number_2 :
+	print(number_1)
+elif number_1 < number_2 :
+	print(number_2)
+
 
