780e0cb3bb99c946a722d9afa7d821f21762ddfe
RomanPrograma <126255710+RomanPrograma@users.noreply.github.com>
2023-03-24 20:50:36 +0200

    ex_1 done

--- boolean/boolean.py
+++ boolean/boolean.py
@@ -1,1 +1,11 @@
 
+first_number = int(input("suggest a number 1   "))
+second_number = int(input("suggest a number 2   "))
+third_number = int(input("suggest a number 3   "))
+value = int(input('\n 1-sum  2-product \n '))
+if value ==1:
+	print(first_number + second_number + third_number)
+elif value ==2:
+	print(first_number * second_number * third_number)
+else :
+	print("error, choose true value")
